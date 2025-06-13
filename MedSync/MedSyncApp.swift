7b79b7b0da41dbbffc4796cfa741bc0b8f77d7bd
pravalgautam <71179696+pravalgautam@users.noreply.github.com>
2025-06-13 09:19:07 +0530

    Initial Commit

---
+++ MedSync/MedSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedSyncApp.swift
+//  MedSync
+//
+//  Created by Praval Gautam on 13/06/25.
+//
+
+import SwiftUI
+
+@main
+struct MedSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
